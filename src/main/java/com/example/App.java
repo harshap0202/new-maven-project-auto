90a3bc90ad8be8b16267ad2c702f0e422c890e7f
harshap0202 <02harshpatil@gmail.com>
2024-07-26 12:07:49 +0530

    main

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Day - 13 Task!");
+        System.out.println("Jenkins Multi-Branch Project");
+        System.out.println("---- Branch : Branch 02 ----");
+    }
+}
